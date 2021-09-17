265211e20611df18a2d9a6fa59114aa5037a511a
annora <annora@LAPTOP-624LMDFD.localdomain>
2021-09-17 15:20:45 +0800

    adds documentation

--- src/App.js
+++ src/App.js
@@ -2,51 +2,76 @@ import './App.css';
 import { Component } from 'react';
 import { render } from '@testing-library/react';
 
-class App extends Component {
- constructor(props){
-   super(props)
-   this.state = {
-      newItem:"",
-      list:[]
-   }
- }
- updateInput(input){
-   this.setState({
-     newItem: input
-   });
- }
- addItem(input){
-   let listArray = this.state.list;
-   listArray.push(input);
-   this.setState({
-     list: listArray,
-     newItem: ""
-   })
- }
- render() {
-   return (
-     <div className="App">
-       <div>
-         <h3>Today's To-Do</h3>
-         <br/>
-         <input
-          type="text"
-          placeholder="Task here"
-          value={this.state.newItem}
-          onChange={(e)=> this.updateInput(e.target.value)}
-         />
-         <button
-           onClick={() => this.addItem(this.state.newItem)}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newItem: "",
+      list: []
+    };
+  }
+  updateInput(key, value) {
+    //keys help react identify which items have been changed
+    this.setState({ [key]: value });
+  }
+  addItem() {
+    // sets a new id for each new list item
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+    // spread syntax to create new object that inherits current list of items
+    const list = [...this.state.list];
+    // add the new item to the list
+    list.push(newItem);
+    // update state with new list, reset the new item input
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+  deleteItem(id) {
+    // copy current list of items
+    const list = [...this.state.list];
+    // filter out the item being deleted
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({ list: updatedList });
+  }
+  render() {
+    return (
+      <div>
+      <h2 className="app-title">TODAY'S TO-DO</h2>
+        <div className="container">
+          Task here
+          <br/>
+          <input
+            type="text"
+            placeholder="Type item here"
+            value={this.state.newItem}
+            onChange={e => this.updateInput("newItem", e.target.value)}
+          />
+          <button
+            className="add-btn btn-floating"
+            onClick={() => this.addItem()}
+            disabled={!this.state.newItem.length}
           >
-          Add
-         </button>
-         <ul>
-           {this.state.list.map( (val)=> <li>{val}</li>)}
-         </ul>
-       </div>
-     </div>
-   );
- }
+            <i class="material-icons"> + </i>
+          </button>
+          <br/> <br/>
+          <ul>
+            {this.state.list.map(item => {
+              return (
+                <li key={item.id}>
+                  {item.value}
+                  <button className="btn btn-floating" onClick={() => this.deleteItem(item.id)}>
+                    <i class="material-icons">x</i>
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
